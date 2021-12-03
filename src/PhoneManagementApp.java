9f4142c13f001373dc22396c819e2737564f5763
Michal Wonia <spodek.mw@gmail.com>
2021-12-03 15:55:42 +0100

    JDBC connector library added. Created a connection with database. Added showAllPhones method to get all records from database.

--- src/PhoneManagementApp.java
+++ src/PhoneManagementApp.java
@@ -1,6 +1,24 @@
+import java.sql.*;
+
 public class PhoneManagementApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/phones", "root", "root");
+        showAllPhones(connection);
+        connection.close();
+    }
 
+    public static void showAllPhones(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM telephones");
+
+        System.out.println("All phones in the database are: ");
+        while (resultSet.next()) {
+            System.out.println(
+                           "Id: " + resultSet.getString("id")
+                    + ", Brand: " + resultSet.getString("brand")
+                    + ", Model: " + resultSet.getString("model")
+                    + ", Color: " + resultSet.getString("color"));
+        }
     }
 }
